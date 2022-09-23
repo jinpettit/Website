62d7a91bdf744768b8a055396495cabed462e9d1
jinpettit <jinpettit@gmail.com>
2022-09-22 19:08:16 -0700

    fixed jquery

--- jquery.js
+++ jquery.js
@@ -6,23 +6,16 @@
     /**
      * Smooth scroll functionality when clicking a link anchor.
      */
-    $('a').on('click', function(e) {
-      var el = $( e.target.getAttribute('href') );
-      var elOffset = el.offset().top;
-      var elHeight = el.height();
-      var windowHeight = $(window).height();
-      var offset;
-  
-      if (elHeight < windowHeight) {
-        offset = elOffset - ((windowHeight / 2) - (elHeight / 2));
-      }
-      else {
-        offset = elOffset;
-      }
-  
-      $.smoothScroll({ speed: 1500 }, offset);
-      return false;
+     $(document).ready(function() {
+      $('a[href*="#"]').on('click', (event) => {
+        const hash = event.currentTarget.hash;
+        if (hash) {
+          event.preventDefault();
+          $('html, body').animate({scrollTop: $(hash).offset().top - 60}, 750);
+        }
+      });
     });
+
   
     /**
      * Enlarges stars when hovering of hard skills.
